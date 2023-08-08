b3008fb996de490d62df8efc19f10ab87f7e3b64
agenceKaiman <141718334+agenceKaiman@users.noreply.github.com>
2023-08-08 16:05:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 corsOptions.credentials = true;
 app.use(cors(corsOptions));
 
-//HTML and Static file
-app.use('/resources', express.static(path.join(__dirname, 'public')));
 
 
 const mongoUri = 'mongodb+srv://yoannchelin:Carambar1703@clusterdashboard.vpng9yi.mongodb.net/?retryWrites=true&w=majority';
